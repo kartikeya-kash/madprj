72363c34c72b4e8fc0fc308d0e9d6d4a3c7b8863
Kartikeya <kartikeya.anjul@gmail.com>
2025-11-04 19:22:02 +0530

    ok

--- MAD_prj/app/src/main/java/com/example/madprj/SignupActivity.java
+++ MAD_prj/app/src/main/java/com/example/madprj/SignupActivity.java
@@ -1,6 +1,8 @@
 package com.example.madprj;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignupActivity extends AppCompatActivity {
+    Button tvLoginobj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        tvLoginobj = findViewById(R.id.tvLogin);
+        tvLoginobj.setOnClickListener(v->{
+            Intent i = new Intent(this, LoginActivity.class);
+            startActivity(i);
         });
+
     }
 }
