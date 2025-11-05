b96258b567edd957c66a8459159d6e23e4e13984
Kartikeya <kartikeya.anjul@gmail.com>
2025-11-05 16:30:40 +0530

    login working

--- MAD_prj/app/src/main/java/com/example/madprj/TrackCaloriesActivity.java
+++ MAD_prj/app/src/main/java/com/example/madprj/TrackCaloriesActivity.java
@@ -7,9 +7,14 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class TrackCaloriesActivity extends AppCompatActivity {
 
     Button btnNext;
+    MaterialCardView card1, card2, card3;
+    int click=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +22,25 @@ public class TrackCaloriesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_track_calories); // your XML file
 
         btnNext = findViewById(R.id.btn_next);
+        card1 = findViewById(R.id.card1);
+        card2 = findViewById(R.id.card2);
+        card3 = findViewById(R.id.card3);
+
 
         btnNext.setOnClickListener(v -> {
-            Intent intent = new Intent(TrackCaloriesActivity.this, LogSleepActivity.class);
-            startActivity(intent);
+         if(click==0){
+             click++;
+             card1.setVisibility(View.GONE);
+             card2.setVisibility(View.VISIBLE);
+         } else if (click==1) {
+             click++;
+             card2.setVisibility(View.GONE);
+             card3.setVisibility(View.VISIBLE);
+         }else if (click==2){
+             Intent intent = new Intent(TrackCaloriesActivity.this, LoginActivity.class);
+             startActivity(intent);
+         }
+
         });
     }
 }
