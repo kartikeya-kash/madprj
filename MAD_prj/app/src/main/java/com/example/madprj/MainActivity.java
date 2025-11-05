564d2782a4e5a8f5e591b2e8f93fb367d84a24d2
Kartikeya <kartikeya.anjul@gmail.com>
2025-11-05 15:59:02 +0530

    given permission

--- MAD_prj/app/src/main/java/com/example/madprj/MainActivity.java
+++ MAD_prj/app/src/main/java/com/example/madprj/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.madprj;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -20,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Find the "Get Started" button
         Button btnGetStarted = findViewById(R.id.btnGetStarted);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (checkSelfPermission(Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 1);
+            }
+        }
 
         // Set click listener to open TrackCaloriesActivity
         btnGetStarted.setOnClickListener(v -> {
